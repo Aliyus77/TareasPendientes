e5be9d753609e26afa615bb3ed3850125c6dafd8
Aliyus77 <angel.ho.rodriguez@gmail.com>
2023-04-20 02:43:59 -0300

    build

--- src/TodoIcon/index.js
+++ src/TodoIcon/index.js
@@ -1,17 +1,18 @@
-import React from "react";
-import { ReactComponets as checked} from './icono.svg';
-import './TodoIcon.css';
+// import React from "react";
+// import { ReactComponets as checked} from './icono.svg';
+// import './TodoIcon.css';
 
 
-function TodoIcon({ type, color, onClick }) {
-    return (
-        <span
-            className={`Icon-container Icon-container--${type}`}
-            onClick={onClick}
-        >
-            {/* SVG */}
-        </span>
-    );
-}
+
+// function TodoIcon({ type, color, onClick }) {
+//     return (
+//         <span
+//             className={`Icon-container Icon-container--${type}`}
+//             onClick={onClick}
+//         >
+//             {/* SVG */}
+//         </span>
+//     );
+// }
 
 export { TodoIcon };
