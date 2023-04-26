4feb6e2dd8bcea41428a771ac8ae878a97337c72
Aliyus77 <angel.ho.rodriguez@gmail.com>
2023-04-26 01:35:24 -0300

    final...nunca, solo por ahora!

--- src/ChangeAlert/withStorageListener.js
+++ src/ChangeAlert/withStorageListener.js
@@ -1,3 +1,4 @@
+// maquetada con ejemplo de higth ordr component
 import React from 'react';
 
 function withStorageListener(WrappedComponent) {
@@ -26,3 +27,31 @@ function withStorageListener(WrappedComponent) {
 }
 
 export { withStorageListener };
+
+
+// React Hooks
+
+// import React from 'react';
+
+// function useStorageListener(sincronize) {
+//   const [storageChange, setStorageChange] = React.useState(false);
+  
+//   window.addEventListener('storage', (change) => {
+//     if (change.key === 'TODOS_V1') {
+//       console.log('Hubo cambios en TODOS_V1');
+//       setStorageChange(true);
+//     }
+//   });
+
+//   const toggleShow = () => {
+//     sincronize();
+//     setStorageChange(false);
+//   };
+
+//   return {
+//     show: storageChange,
+//     toggleShow,
+//   };
+// }
+
+// export { useStorageListener };
