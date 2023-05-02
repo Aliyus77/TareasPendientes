1bffbebbb6975f1550275ca06194b4dffc9fb5f6
Aliyus77 <angel.ho.rodriguez@gmail.com>
2023-05-02 00:49:13 -0300

    finalizado?

--- src/App/useLocalStorage.js
+++ src/App/useLocalStorage.js
@@ -1,41 +1,98 @@
 import React from "react";
+
 function useLocalStorage(itemName, initialValue) {
-  const [syncronizedItem, setSyncronizedItem] = React.useState(true);
-  const [error, setError] = React.useState(false);
-  const [loading, setLoading] = React.useState(true);
-  const [item, setItem] = React.useState(initialValue);
+  const [state, dispatch] = React.useReducer(reducer, initialState({ initialValue }) );
+  const { 
+    syncronizedItem,
+    error,
+    loading,
+    item,
+  } = state;
+
+  // ACTIOMS CREATORS
+
+  const onError = (error) => dispatch({ type: actionTypes.error, payload: error });
+  const onSuccess = (item) => dispatch({ type: actionTypes.success, payload: item });
+  const onSave = (item) => dispatch({ type: actionTypes.save, payload: item });
+  const onSyncronize = () => dispatch({ type: actionTypes.syncronize });
+  
   React.useEffect(() => {
     setTimeout(() => {
       try {
         const localStorageItem = localStorage.getItem(itemName);
         let parsedItem;
+
         if (!localStorageItem) {
           localStorage.setItem(itemName, JSON.stringify(initialValue));
           parsedItem = initialValue;
         } else {
           parsedItem = JSON.parse(localStorageItem);
         }
-        setItem(parsedItem);
-        setLoading(false);
-        setSyncronizedItem(true);
-      } catch (error) {
-        setError(error);
+
+        onSuccess(parsedItem);
+        
+      } catch(error) {
+        onError(error);
       }
     }, 3000);
   }, [syncronizedItem]);
+
   const saveItem = (newItem) => {
     try {
       const stringifiedItem = JSON.stringify(newItem);
       localStorage.setItem(itemName, stringifiedItem);
-      setItem(newItem);
-    } catch (error) {
-      setError(error);
+      onSave(newItem);
+    } catch(error) {
+      onError(error);
     }
   };
-  const syncronizeItem = () => {
-    setLoading(true);
-    setSyncronizedItem(false);
+
+  const syncronizeItem = () => { 
+    onSyncronize();
   };
+
   return { item, saveItem, loading, error, syncronizeItem };
 }
+
+const initialState = ({ initialValue }) => ({
+  syncronizedItem: true,
+  error: false,
+  loading: true,
+  item: initialValue,
+});
+
+const actionTypes = {
+  error: 'ERROR',
+  success: 'SUCCESS',
+  save: 'SAVE',
+  syncronize: 'SYNCRONIZE',
+};
+
+const reducerObject = (state, payload) => ({
+  [actionTypes.error]: {
+    ...state,
+    error: true,
+  },
+  [actionTypes.save]: {
+    ...state,
+    item: payload,
+  },
+  [actionTypes.success]: {
+    ...state,
+    error: false,
+    loading: false,
+    syncronizedItem: true,
+    item: payload,
+  },
+  [actionTypes.syncronize]: {
+    ...state,
+    syncronizedItem: false,
+    loading: true,
+  },
+});
+
+const reducer = (state, action) => {
+  return reducerObject(state, action.payload)[action.type] || state;
+};
+
 export { useLocalStorage };
--- src/ChangeAlert/index.js
+++ src/ChangeAlert/index.js
@@ -1,3 +1,5 @@
+// basado en HOC de withStorageListener
+
 import React from 'react';
 import { withStorageListener } from './withStorageListener';
 import './ChangeAlert.css';
@@ -26,3 +28,34 @@ function ChangeAlert({ show, toggleShow }) {
 const ChangeAlertWithStorageListener = withStorageListener(ChangeAlert);
 
 export { ChangeAlertWithStorageListener };
+
+// con React Hooks
+
+// import React from 'react';
+// import { useStorageListener } from './useStorageListener';
+// import './ChangeAlert.css';
+
+// function ChangeAlert({ sincronize }) {
+//   const { show, toggleShow } = useStorageListener(sincronize);
+  
+//   if (show) {
+//     return (
+//       <div className="ChangeAlert-bg">
+//         <div className="ChangeAlert-container">
+//           <p>Parece que cambiaste tus TODOs en otra pestaÃ±a o ventana del navegador.</p>
+//           <p>Â¿Quieres sincronizar tus TODOs?</p>
+//           <button
+//             className="TodoForm-button TodoForm-button--add"
+//             onClick={toggleShow}
+//           >
+//             Yes!
+//           </button>
+//         </div>
+//       </div>
+//     );
+//   } else {
+//     return null;
+//   }
+// }
+
+// export { ChangeAlert };
